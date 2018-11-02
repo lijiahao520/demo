9d5dcacb5e14c14804724a475f6dd97a0d58ade2
lijiahao <1990494325@qq.com>
2018-11-02 15:58:44 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+      
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body,html{
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  width: 80px;
+  height: 100%;
+  background: green;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li>
+        <i class="icon-gouwuche iconfont"></i>
+        收银
+        </li>
+      <li><i class="icon-gouwuche iconfont"></i>商品</li>
+      <li><i class="icon-gouwuche iconfont"></i>店铺</li>
+      <li><i class="icon-gouwuche iconfont"></i>VIP</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+.iconfont{
+  display: block;
+  font-size: 200%;
+}
+li {
+  text-align: center;
+  display: block;
+  width: 100%;
+  height: 100px;
+  /* line-height: 100px; */
+}
+a {
+  color: #42b983;
+}
+</style>
